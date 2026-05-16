4a5de7d41e4da8b9ace0c735f8eff0d9582069d7
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-05-16 13:06:25 +0200

    and do not reverse the sweep

--- test_oversampling.jl
+++ test_oversampling.jl
@@ -3,7 +3,7 @@ m_over = Flux.Chain([Flux.Chain(Flux.Conv(l.weight, l.bias), x -> DSP.resample(t
 WAV.wavwrite(m_over((test[:,:,:] .- x_mean) ./ x_scale) .* y_scale .+ y_mean, "$(outpath)/oversampled_$(test_file_name).wav"; Fs=test_fs)
 
 sweep, sweep_fs = WAV.wavread("data/sweep.wav")
-sweep = Float32.(reverse(sweep))
+sweep = Float32.(sweep)
 
 WAV.wavwrite(m_over((sweep[:,:,:] .- x_mean) ./ x_scale) .* y_scale .+ y_mean, "$(outpath)/oversampled_sweep.wav"; Fs=test_fs)
 
