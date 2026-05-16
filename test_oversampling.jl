14670a44d337f454a91521c4cb832775d4a202e9
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-05-16 13:00:27 +0200

    add sweeps

--- test_oversampling.jl
+++ test_oversampling.jl
@@ -1,4 +1,13 @@
-m_over = Flux.Chain([Flux.Chain(Flux.Conv(l.weight, l.bias), x -> DSP.resample(tanh.(DSP.resample(x, 8, dims=1)), 1/8, dims=1)[:,:,:]) for l in m[1:(end-1)]]..., Flux.Conv(m[end].weight, m[end].bias)) |> cpu
+m_over = Flux.Chain([Flux.Chain(Flux.Conv(l.weight, l.bias), x -> DSP.resample(tanh.(DSP.resample(x, 8, dims=1)), 1/8, dims=1)[:,:,:]) for l in m_min[1:(end-1)]]..., Flux.Conv(m[end].weight, m_min[end].bias)) |> cpu
 
 WAV.wavwrite(m_over((test[:,:,:] .- x_mean) ./ x_scale) .* y_scale .+ y_mean, "$(outpath)/oversampled_$(test_file_name).wav"; Fs=test_fs)
+
+sweep, sweep_fs = WAV.wavread("data/sweep.wav")
+sweep = Float32.(reverse(sweep))
+
+WAV.wavwrite(m_over((sweep[:,:,:] .- x_mean) ./ x_scale) .* y_scale .+ y_mean, "$(outpath)/oversampled_sweep.wav"; Fs=test_fs)
+
+WAV.wavwrite(cpu(m_min((dev(sweep[:,:,:]) .- x_mean) ./ x_scale) .* y_scale .+ y_mean), "$(outpath)/sweep.wav"; Fs=test_fs)
+
+
 
