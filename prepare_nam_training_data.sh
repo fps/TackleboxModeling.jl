a6a0a8f7a277d5a831924c0a7af62883d4df14a1
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-05-17 10:34:51 +0200

    add hiir oversampling library

--- prepare_nam_training_data.sh
+++ prepare_nam_training_data.sh
@@ -1,4 +1,7 @@
 # nam_wavenet is an example program shipped with anna: https://github.com/fps/anna
 
-nam_wavenet data/"$1"/model.nam  data/nam_training_input.wav data/"$1"/nam_training_output.wav
-nam_wavenet data/"$1"/model.nam  data/"Take1_Audio 1-1_short.wav" data/"$1"/"nam_Take1_Audio 1-1_short.wav"
+set -e
+
+nam_wavenet data/"$1"/model.nam  data/nam_training_input.wav data/"$1"/"nam_training_output.wav"
+nam_wavenet data/"$1"/model.nam  data/"Take1_Audio 1-1_short.wav" data/"$1"/"nam_Take1_Audio 1-1_short_output.wav"
+nam_wavenet data/"$1"/model.nam  data/sweep.wav data/"$1"/nam_sweep.wav
