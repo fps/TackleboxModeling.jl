a6f427de7be9bc7775417bbd4cba729f1d11ee34
Florian Paul Schmidt <mista.tapas@gmx.net>
2026-05-28 14:44:16 +0200

    add EVH 5150 example

--- prepare_nam_training_data.sh
+++ prepare_nam_training_data.sh
@@ -3,5 +3,5 @@
 set -e
 
 nam_wavenet data/"$1"/model.nam  data/nam_training_input.wav data/"$1"/"nam_training_output.wav"
-nam_wavenet data/"$1"/model.nam  data/"Take1_Audio 1-1_short.wav" data/"$1"/"nam_Take1_Audio 1-1_short_output.wav"
+nam_wavenet data/"$1"/model.nam  data/"Take1_Audio 1-1_short.wav" data/"$1"/"nam_Take1_Audio 1-1_short.wav"
 nam_wavenet data/"$1"/model.nam  data/sweep.wav data/"$1"/nam_sweep.wav
